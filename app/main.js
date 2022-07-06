13a436ad3b9a96a380be9ee7fa4ae2a6f498c3a0
CABON-TECH <kishushubonifacembogho@gmail.comm>
2022-07-06 18:21:51 +0300

    initial

--- app/main.js
+++ app/main.js
@@ -1,1 +1,12 @@
-main.js
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    navbar.classList.toggle('active');
+    menu.classList.toggle('fa-times');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+}
